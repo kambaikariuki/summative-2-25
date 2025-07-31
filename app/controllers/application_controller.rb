233c8df3ae1bc1fe64c57e265a068bdc052fee27
kambaikariuki <kariukikambai@gmail.com>
2025-07-31 21:05:10 +0300

    fixup! Minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+  devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :first_name, :last_name, :email, :password, :password_confirmation
+  ])
+
+  devise_parameter_sanitizer.permit(:account_update) do |user_params|
       if current_user&.admin?
         user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
       else
@@ -18,6 +22,7 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
   def check_if_admin
     redirect_to root_path unless current_user.admin?
   end
